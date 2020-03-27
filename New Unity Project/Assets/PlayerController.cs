5c5b160ad13631d7f1f40b2a6c7bc76dc3cd87c0
Soròs <luc.rossicone@stud.uniroma3.it>
2020-03-27 16:11:14 +0100

    first

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+    }
+}
