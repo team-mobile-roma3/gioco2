d83348a4edde2147e9e69335c98a7358e0cd5911
v0rren <77diana1992@gmail.com>
2020-03-28 16:08:58 +0100

    fine video 1

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
     Rigidbody2D rigidbody;
-
+    public Text collectedText;
+    public static int collectedAmount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+         rigidbody =  GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+        rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed);
+        collectedText.text = "Items Collected " + collectedAmount;
     }
 }
