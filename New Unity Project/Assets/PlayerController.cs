e50c9a3b2be4ba3df7a7ef452416318a8404a02d
Luca Rossicone <61837246+LR-Cumulus@users.noreply.github.com>
2020-03-29 11:15:36 +0200

    fattoVideo2

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -9,6 +9,14 @@ public class PlayerController : MonoBehaviour
     public Text collectedText;
     public static int collectedAmount = 0;
 
+    public GameObject bulletPrefab;
+
+    public float bulletSpeed;
+
+    private float lastFire;
+
+    public float fireDelay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +29,26 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVert = Input.GetAxis("ShootVertical");
+
+        if ((shootHor != 0 || shootVert != 0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHor,shootVert);
+            lastFire = Time.time;
+        } 
         rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed);
         collectedText.text = "Items Collected " + collectedAmount;
+    }
+    
+    void Shoot(float x, float y) 
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x<0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y<0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
+        );
     }
 }
