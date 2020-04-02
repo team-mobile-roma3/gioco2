af548e3eb0fcc2c2ba0eff39f7c22256e5351ac9
Luca <luc.rossicone@stud.uniroma3.it>
2020-04-02 13:10:26 +0200

    fattoVideo5

---
+++ New Unity Project/Assets/Scripts/HealthUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject heartContainer;
+
+    private float fillValue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fillValue = (float)GameController.Health;
+        fillValue = fillValue/GameController.MaxHealth;
+        heartContainer.GetComponent<Image>().fillAmount = fillValue;
+    }
+}
