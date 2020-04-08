202cbed5aa924013b0ece2c8f8752798d4c111ce
v0rren <77diana1992@gmail.com>
2020-04-08 03:48:19 +0200

    parte 4

--- New Unity Project/Assets/Scripts/DungeonGeneration/DungeonCrawlerController.cs
+++ New Unity Project/Assets/Scripts/DungeonGeneration/DungeonCrawlerController.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum Direction
+{
+    up = 0,
+    left = 1,
+    right = 3,
+    down = 2
+};
 
 public class DungeonCrawlerController : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public static List<Vector2Int> positionVisited = new List<Vector2Int>();
+    private static readonly Dictionary<Direction, Vector2Int> directionMovementMap = new Dictionary<Direction, Vector2Int>
+    {
+        {Direction.up, Vector2Int.up },
+        {Direction.left, Vector2Int.left },
+        {Direction.right, Vector2Int.right },
+        {Direction.down, Vector2Int.down },
+    };
+
+    public static List<Vector2Int> GenerateDungeon(DungeonGenerationDATA dungeonData)
+    {
+        List<DungeonCrawler> dungeonCrawlers = new List<DungeonCrawler>();
+
+        for (int i = 0; i < dungeonData.numberOfCrawlers; i++){
+            dungeonCrawlers.Add(new DungeonCrawler(Vector2Int.zero));
+        }
+
+        int iterations = Random.Range(dungeonData.iterationMin, dungeonData.iterationMax);
+
+        for ( int i = 0; i < iterations; i++)
+        {
+            foreach (DungeonCrawler dungeonCrawler in dungeonCrawlers)
+            {
+                Vector2Int newPos = dungeonCrawler.Move(directionMovementMap);
+                positionVisited.Add(newPos);
+            }
+
+        }
+        return positionVisited;
     }
 }
