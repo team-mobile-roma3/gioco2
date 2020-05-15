e2ff7752c007242f75fd65fd93c8f779eee70df7
v0rren <77diana1992@gmail.com>
2020-05-15 12:52:04 +0200

    items bs

---
+++ New Unity Project/Assets/BsItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BsItem : MonoBehaviour
+{
+    public int price;
+    public GameObject item;
+    
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Inventory.CoinsCollected >= price)
+        {
+
+            Inventory.CoinsCollected -= price;
+            item = Instantiate(item.gameObject, transform.position, Quaternion.identity) as GameObject;
+            Destroy(gameObject);
+        }
+
+    }
+}
