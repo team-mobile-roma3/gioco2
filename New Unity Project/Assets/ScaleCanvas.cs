00e1264d37bbc04c01ff0ae431c1e30c5c441859
v0rren <77diana1992@gmail.com>
2020-05-21 00:26:43 +0200

    arifix

---
+++ New Unity Project/Assets/ScaleCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleCanvas : MonoBehaviour
+{
+    void Awake()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+
+        Vector3 screenSize = Camera.main.ViewportToWorldPoint(Vector3.up + Vector3.right);
+
+        screenSize *= 02;
+
+        float sizeY = screenSize.y / 30;
+        float sizeX = screenSize.x / 30;
+
+        rt.localScale = new Vector3(sizeX, sizeY, 1);
+    }
+}
